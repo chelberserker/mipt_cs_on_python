69933e6a30d21608bc8d6cefbbdf1989e16a74f4
chelberserker <bersenjow@gmail.com>
2015-11-03 11:47:21 +0300

    ex 10

---
+++ lab10/EX5.py
@@ -0,0 +1,30 @@
+def createbackdict(s):
+    simpledict = dict()
+    enword = s[:s.index('\t')]
+    russianwords = s[s.index('\t')+3:]
+    for i in range(len(russianwords)):
+    if (s[i]=='.')and(s[i]==',')and(s[i]=='!')and(s[i]=='?'):
+        s[i] = ' '
+    word = ''
+    for i in range(len(russianwords)):
+        if s[i] != ' ':
+            word += s[i]
+        else:
+            simpledict[word]=enword
+            word = ''
+    return simpledict
+
+inp = open('input.txt', 'r')
+output = open('output.txt', 'w')
+dictionary = dict()
+while line != '':
+    line = inp.readline()
+    sdic = createbackdict(line)
+    dictionary.update(sdic)
+    
+for i in range(len(dictionary)):
+    #should be sorted
+    
+    
+    
+        
