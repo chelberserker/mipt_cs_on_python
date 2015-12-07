02c47612d70f21311b7a8e0037d7253365855477
chelberserker <bersenjow@gmail.com>
2015-12-07 19:15:01 +0300

    Update EX4.py

--- lab10/EX4.py
+++ lab10/EX4.py
@@ -1,30 +1,28 @@
-def creatingdictionary(f):
-    Dictionary = dict()
-    while line != '':
-        line = inp.readline()
-        enword = s[:s.index('\t')]
-        rword = s[s.index('\t')+3:]
-        Dictionary[enword]=rword
-    return Dictionary
-
-def translation(s, Dict):
-    translated = ''
-    word = '' 
-    for i in range(len(s)):
-        if s[i] != ' ':
-            word += s[i]
-        else:
-            if word in Dict:
-                translated += Dict[word]
-            else:
-                translated += word
-            word = ''
-    return translated
-
-inp = open('input.txt', 'r')
-output = open('output.txt', 'w')
-dictionary = creatingdictionary(inp)
-while line != '':
-    line = inp.readline()
-    print(line)
-    print(translation(line, dictionary, file=output))
+with open('en-ru.txt', 'r') as dictionary:
+    with open('input.txt', 'r') as inp:
+        with open('output.txt', 'w') as out:
+            dict = {}
+            dictline = dictionary.readline()
+            while dictline != '':
+                dictline = dictline.split('\t-\t')
+                dictline[1] = dictline[1].split('\n')[0]
+                dict[dictline[0]] = dictline[1]
+                dictline = dictionary.readline()
+            line = inp.readline()
+            for x in dict:
+                print(x, dict[x])
+            while line != '':
+                line = list(line)
+                word = ''
+                for i in range(len(line)):
+                    char = line[i]
+                    if ord(char) < 65 or 90 < ord(char) < 97 or ord(char) > 122:
+                        word = word.lower()
+                        if word in dict and dict[word] != '=':
+                            word = dict[word]
+                        print(word, end='', file=out)
+                        print(char, end='', file=out)
+                        word = ''
+                    else:
+                        word += char
+                line = inp.readline()
